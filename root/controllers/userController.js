48f3037d3d3e85c2ec27cf9d879fcd274de201aa
chrismuto <chrismuto@gmail.com>
2024-10-04 12:43:16 -0700

    finished user controller

--- root/controllers/userController.js
+++ root/controllers/userController.js
@@ -1,76 +1,75 @@
-import users from "../model/user.json" assert { type: "json"}
-import fs from "fs"
+import User from "../model/User.js";
 
 //Move all functions into data, export for use in jest/calls
 //Once all methods are in data, move to separate file and import to all controllers for cleaner code
 //get rid of all instances of data.users
-const data = {
-    users,
-    setUsers: function (newData) {
-        this.users = newData;
-        fs.writeFileSync("model/user.json", JSON.stringify(newData, null, 2));
-    },
-    getUser: function (reqId) {
-        return this.users.find(user => user.id === parseInt(reqId));
-    },
-    getAllUsers: function () {
-        return this.users;
-    },
-    deleteUser: function (reqId) {
-        return this.users.find(user => user.id === parseInt(reqId));
-    },
-    filterById: function (reqId) {
-        return this.users.filter(targetUser => targetUser.id !== parseInt(reqId));
-    },
-};
 
-const getAllUsers = (req, res) => {
-    res.json(data.getAllUsers());
+const getAllUsers = async (req, res) => {
+    const users = await User.find();
+    if (!users) return res.status(204).json({ 'message': 'no employees found' });
+    res.json(users);
 }
 
-const createNewUser = (req, res) => {
-        const newUser = {
-            id: data.getAllUsers()[data.getAllUsers().length -1].id + 1 || 1,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname
-        }
+const createNewUser = async (req, res) => {
+    if (!req?.body.username || !req?.body.password) {
+        return res.status(400).json({ 'message': 'username and password are required '})
+    }
         
-        if (!newUser.firstname || !newUser.lastname) {
-            return res.status(400).json({ 'message': 'users require a first and last name '});
+    try {
+        const result = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        res.status(201).json(result);
+    } catch (err) {
+        console.log(err)
+    }
+}
+    
+    const updateUser = async (req, res) => {
+        if (!req?.body?.id) {
+            return res.status(400).json({ 'message': 'ID parameter is required' });
         }
+
+        const user = await User.findOne( { _id: req.body.id }).exec();
+
+        if (!user) {
+            return res.status(204).json({ 'message': `No user with ID ${req.body.id}` });
+        }
+
+        const duplicate = await User.findOne( { username: user }).exec();
+
+        if (duplicate) return res.status(409).json({ "message": "username is already taken" });
         
-        data.setUsers([...data.getAllUsers(), newUser]);
-        res.status(201).json(data.getAllUsers());
+        if(req.body?.username) user.username = req.body.username;
+        const result = user.save();
+        res.status(201).json(result);
     }
     
-    const updateUser = (req, res) => {
-        const user = data.getUser(req.body.id);
+    const deleteUser = async (req, res) => {
+
+        if (!req?.body?.id) return res.status(400).json({ 'message': 'user ID required'});
+
+        const user = await User.findOne( { _id: req.body.id }).exec();
         if (!user) {
-            return res.status(400).json({ "message": `User ID ${req.body.id} not found` });
+            return res.status(400).json({ "message": `No user with ID ${req.body.id}` });
         }
-        if(req.body.firstname) user.firstname = req.body.firstname;
-        if(req.body.lastname) user.lastname = req.body.lastname;
-        const filteredArray = data.filterById(req.body.id);
-        const unsortedArray = [...filteredArray, user];
-        data.setUsers(unsortedArray.sort((a, b) => a.id > b.id ? 1 : a.id < b.id ? -1 : 0));
-        res.status(201).json(data.users);
+        const result = await User.deleteOne({ _id: req.body.id });
+        
+        res.json(result);
     }
     
-    const deleteUser = (req, res) => {
-        const user = data.deleteUser(req.body.id);
+    const getUser = async (req, res) => {
+
+        if (!req?.params?.id) return res.status(400).json({ 'message': 'user ID required'});
+
+        const user = await User.findOne({ _id: req.params.id });
+
         if (!user) {
-            return res.status(400).json({ "message": `User ID ${req.body.id} not found` });
-        }
-        const filteredArray = data.filterById(req.body.id);
-        data.setUsers([...filteredArray]);
-        res.status(201).json(users);
-    }
-    
-    const getUser = (req, res) => {
-        const user = data.getUser(req.body.id);
-        if (!user) {
-            return res.status(400).json({ "message": `User ID ${req.body.id} not found for this id` });
+            return res.status(400).json({ "message": `No user with ID ${req.params.id}` });
         };
+
         res.json({ user });
     }
 
--- root/routes/api/user.js
+++ root/routes/api/user.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.route('/')
     .get(verifyJWT, userController.getAllUsers)
-    .post(userController.createNewUser) //move to authController so that this file can be hidden behind verifyJWT on main server file?
+    .post(userController.createNewUser)
     .put(verifyJWT, userController.updateUser)
     .delete(verifyJWT, userController.deleteUser);
 
